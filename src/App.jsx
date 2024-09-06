1a1e5513b4450fa3cef8167b5c4c71349970da49
Amin Mahmidi <mahmidismart@outlook.com>
2024-09-06 12:18:03 +0330

    fixing copy message app and added disabled btn effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import { Moon, Sun, Copy } from "@phosphor-icons/react";
+import {
+  Moon,
+  Sun,
+  Copy,
+  CheckCircle,
+  WarningCircle,
+} from "@phosphor-icons/react";
 function App() {
   const [text, setText] = useState("");
   const [wordCount, setWordCount] = useState(0);
   const [charCount, setCharCount] = useState(0);
+  const [copy, setCopy] = useState(false);
   const [isToggle, setIsToggle] = useState(false);
   const changeHandler = (event) => {
     setText(event.target.value);
@@ -21,7 +28,7 @@ function App() {
     setCharCount(text.length);
   }, [text]);
   const wordTimer = text.split(/\s+/).length;
-  const averageWPM = 200;
+  const averageWPM = 90;
   const estimatedReadingTime = Math.ceil(wordTimer / averageWPM);
   const toggle = () => {
     setIsToggle(!isToggle);
@@ -35,29 +42,41 @@ function App() {
       setIsToggle(savedMode === "true");
     }
   }, []);
-
   const clipboard = async () => {
     if (navigator.clipboard && text.length > 0) {
       try {
-        await navigator.clipboard.writeText(text)
+        await navigator.clipboard.writeText(text);
+        setCopy(true);
+        setTimeout(() => {
+          setCopy(false);
+        }, 4000);
       } catch (error) {
-        
+        alert("خطا");
       }
+    } else {
+      setCopy(false);
     }
-    else {
-      alert('ne text')
-    }
-  }
+  };
   return (
     <>
       <div className={isToggle ? " container light" : "container dark"}>
         <section className="text-container">
           <div className="top-section">
-            <h3>شمارنده تعداد کارکترها به دقایق</h3>
+            <h4>بنویس</h4>
             <div className="top-section-btn">
-              <span className="copy-success"> متن کپی شد </span>
-              <span className="copy-success"> فرم خالی است !! </span>
-              <button onClick={clipboard} type="button" className="copy-btn">
+              <div className="copy-message">
+                {copy && (
+                  <span className="show-copy">
+                    <CheckCircle /> متن کپی شد
+                  </span>
+                )}
+              </div>
+              <button
+                onClick={clipboard}
+                type="button"
+                disabled={text.length === 0}
+                className="copy-btn"
+              >
                 <Copy size={18} weight="bold" /> کپی متن
               </button>
               <button type="button" className="toggle" onClick={toggle}>
@@ -81,9 +100,6 @@ function App() {
               }
             >
               <p className="word-count"> کاراکتر: {wordCount}</p>
-              {/* <button type="button">
-              شمارش وقت
-            </button> */}
               {wordTimer > 0 && (
                 <p>زمان تقریبی مطالعه: {estimatedReadingTime}دقیقه </p>
               )}
